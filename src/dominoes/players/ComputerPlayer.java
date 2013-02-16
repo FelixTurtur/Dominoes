cc52d8e310376722b4ca688ea75b04d6d01249b9
Nick <git@greengrate.co.uk>
2013-02-16 22:48:09 +0000

    Added Player and ComputerPlayer classes
    Player class to implement the DominoPlayer interface
    Setters and getters for the Player class (name, score, etc)
    Have used a linked list for the hand
    makePlay has not been implemented - plan is to override this function in
    the ComputerPlayer class (which extends Player).
    Not tested yet!

---
+++ src/dominoes/players/ComputerPlayer.java
@@ -0,0 +1,17 @@
+package dominoes.players;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nick
+ * Date: 16/02/13
+ * Time: 21:33
+ */
+public class ComputerPlayer extends Player {
+
+    public dominoes.Play makePlay(dominoes.Table table) throws dominoes.CantPlayException{
+           //not implemented yet
+        return null;
+    }
+
+
+}
---
+++ src/dominoes/players/Player.java
@@ -0,0 +1,68 @@
+package dominoes.players;
+
+import com.sun.org.apache.bcel.internal.generic.IFNONNULL;
+import dominoes.Bone;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nick
+ * Date: 16/02/13
+ * Time: 21:54
+ */
+public class Player implements DominoPlayer {
+
+    private String name;
+    private int points;
+    protected List<Bone> hand = new LinkedList<Bone>();
+
+
+    public dominoes.Play makePlay(dominoes.Table table) throws dominoes.CantPlayException{
+        //not implemented yet
+        return null;
+    }
+
+    public void takeBack(dominoes.Bone bone){
+        hand.add(bone);
+    }
+
+    public void draw(dominoes.BoneYard boneYard){
+        Bone newBone=boneYard.draw();
+        if ( newBone != null ){
+            hand.add(newBone);
+        }
+    }
+
+    public int numInHand(){
+         return hand.size();
+    }
+
+    public dominoes.Bone[] bonesInHand(){
+        Bone[] b = hand.toArray(new Bone[0]);
+        return b;
+    }
+
+    public void newRound(){
+         hand.clear();
+    }
+
+    public void setPoints(int i){
+        points = i;
+    }
+
+    public int getPoints(){
+        return points;
+    }
+
+    public void setName(java.lang.String s){
+        name=s;
+    }
+
+    public java.lang.String getName(){
+        return name;
+    }
+
+
+}
