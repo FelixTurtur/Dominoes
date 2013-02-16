2bc962df00fd05cd613a234377512fd983a06a30
Nick <git@greengrate.co.uk>
2013-02-16 23:34:23 +0000

    Additions to UI class

--- src/dominoes/UI.java
+++ src/dominoes/UI.java
@@ -8,15 +8,22 @@ package dominoes;
  */
 public class UI implements DominoUI {
     public void display(dominoes.players.DominoPlayer[] dominoPlayers, dominoes.Table table, dominoes.BoneYard boneYard){
-
+        System.out.println("Not implemented yet" );
+        System.out.println(boneYard.size() + "bones left in boneyard");
 
     }
 
     public void displayRoundWinner(dominoes.players.DominoPlayer dominoPlayer){
+        System.out.println(dominoPlayer.getName() + "wins the round!" );
 
     }
 
     public void displayInvalidPlay(dominoes.players.DominoPlayer dominoPlayer){
+        System.out.println("Invalid Play");
+    }
 
+
+    private void drawBone(Bone b){
+        System.out.println("[" + b.left() + "|" + b.right() +"]" );
     }
 }
