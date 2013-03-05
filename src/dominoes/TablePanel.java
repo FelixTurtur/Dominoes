150b5d917c4948fbf348a2ef4b6f9bf06fa8ab2e
Timothy Baldock <tb@entropy.me.uk>
2013-03-05 21:27:55 +0000

    Refactor TablePanel and create BoneDrawer class

--- src/dominoes/TablePanel.java
+++ src/dominoes/TablePanel.java
@@ -16,15 +16,11 @@ public class TablePanel extends JPanel {
     // TODO sort out repeated code - eg. size variable and the drawPips method
     // TODO Make a class to derive TablePanel and PlayerHandPanel from!!!!!!!!!!
     Table table;
-    static int size=120;
-
 
     public TablePanel(FlowLayout flowLayout){
         super(flowLayout);
 
     }
-
-
 
     public void setTable(Table t){
         table=t;
@@ -35,9 +31,6 @@ public class TablePanel extends JPanel {
         drawTable(graphics);
 
     }
-
-
-
 
     private void drawTable(Graphics graphics){
         //Draws the line of dominoes that have so far been played this round.
@@ -53,7 +46,8 @@ public class TablePanel extends JPanel {
                 drawDashes(10 + 130 * n, y + 30, graphics);
             }
             else{
-                drawBone(10 + 130 * n, y, bones[i], graphics);
+                BoneDrawer boneDrawer = new BoneDrawer(bones[i]);
+                boneDrawer.drawBone(10 + 130 * n, y, graphics);
             }
             System.out.print(" ");
         }
@@ -61,70 +55,11 @@ public class TablePanel extends JPanel {
         System.out.println("");
     }
 
-    public void drawBone(int x,int y,Bone bone,Graphics graphics){
-
-        //TODO get it to draw something else for non human player type
-        graphics.setColor(Color.black);
-        graphics.fillRoundRect(x, y, size,size/2,size/20,size/20);
-        drawPips(bone.left(),x,y,graphics);
-        graphics.setColor(Color.white);
-        graphics.drawLine(x+size/2, y, x+size/2, y+size/2);
-        drawPips(bone.right(),x+size/2,y,graphics);
-    }
-
-
-
-
     private void drawDashes(int x, int y,Graphics graphics){
         for (int i=0;i<5;i=i+2){
             graphics.setColor(Color.black);
             graphics.fillRoundRect(10+x+20*i,y,20,5,2,2);
 
-        }
-    }
-
-    private void drawPips(int num,int x,int y,Graphics graphics){
-
-        switch(num){
-            case 1:
-                graphics.setColor(Color.green);
-                graphics.fillOval(x+25, y+25, size/12, size/12);
-                break;
-            case 2:
-                graphics.setColor(Color.blue);
-                graphics.fillOval(x+10, y+10, size/12, size/12);
-                graphics.fillOval(x+40, y+40, size/12, size/12);
-                break;
-            case 3:
-                graphics.setColor(Color.pink);
-                graphics.fillOval(x+10, y+10, size/12, size/12);
-                graphics.fillOval(x+25, y+25, size/12, size/12);
-                graphics.fillOval(x+40, y+40, size/12, size/12);
-                break;
-            case 4:
-                graphics.setColor(Color.red);
-                graphics.fillOval(x+10, y+10, size/12, size/12);
-                graphics.fillOval(x+40, y+10, size/12, size/12);
-                graphics.fillOval(x+10, y+40, size/12, size/12);
-                graphics.fillOval(x+40, y+40, size/12, size/12);
-                break;
-            case 5:
-                graphics.setColor(Color.cyan);
-                graphics.fillOval(x+25, y+25, size/12, size/12);
-                graphics.fillOval(x+10, y+10, size/12, size/12);
-                graphics.fillOval(x+40, y+10, size/12, size/12);
-                graphics.fillOval(x+10, y+40, size/12, size/12);
-                graphics.fillOval(x+40, y+40, size/12, size/12);
-                break;
-            case 6:
-                graphics.setColor(Color.yellow);
-                graphics.fillOval(x+10, y+10, size/12, size/12);
-                graphics.fillOval(x+25, y+10, size/12, size/12);
-                graphics.fillOval(x+40, y+10, size/12, size/12);
-                graphics.fillOval(x+10, y+40, size/12, size/12);
-                graphics.fillOval(x+25, y+40, size/12, size/12);
-                graphics.fillOval(x+40, y+40, size/12, size/12);
-                break;
         }
     }
 }
