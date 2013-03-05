3c94828751d8b18de2f50501d8452aaf47627eef
Nick <git@greengrate.co.uk>
2013-03-05 01:24:35 +0000

    Major graphics rework
    Merged Abbie's FramePanels into Artist. Then split out a couple of
    derived types of JPanels.
    
    Abbie - the key bit was overriding the JPanel's paint method with a
    version that added the bits we needed and then calls the parent class'
    version.
    
    Next thing to do is to sort out my two additional classes -
    PlayerHandPanel and TablePanel. There is a load of repeated code so they
    should be derived from a shared parent class.
    
    Note all my reworks have killed the text output we had. Can always put
    it back in - preferably as a class separate to the graphics ones.

--- src/dominoes/players/PossiblePlay.java
+++ src/dominoes/players/PossiblePlay.java
@@ -14,11 +14,11 @@ public class PossiblePlay {
     private Play play;
     private boolean flipped = false;
     private int score; //used for computer player routine to score the possible moves to pick a better option
-    private static int doubleWeighting=5; //bias to add if the bone is a double since it is harder to get rid of
+    private static int doubleWeighting=2; //multiple to apply to weight of bones which are doubles - these are harder to get rid of.
 
     PossiblePlay(Bone bone, int end, boolean flip) {
         score=bone.right()+bone.left();
-        if (bone.right()==bone.left()) score=score+doubleWeighting;
+        if (bone.right()==bone.left()) score=score*doubleWeighting;
         flipped = flip;
         play = new Play(bone, end);
     }
