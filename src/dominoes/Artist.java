955e1f4fcf7eb1f8b908bb25e23489ed6df31a38
Nick <git@greengrate.co.uk>
2013-03-10 20:06:12 +0000

    Player hands now displayed portrait

--- src/dominoes/Artist.java
+++ src/dominoes/Artist.java
@@ -66,7 +66,7 @@ public class Artist extends JFrame implements ActionListener {
 
     private void setupPlayerHand(PlayerHandPanel panel, EtchedBorder eb1) {
         panel.setBackground(Color.gray);
-        panel.setSize(windowWidth,windowHeight/5);
+        panel.setSize(windowWidth,windowHeight/4);
         panel.setBorder(eb1);
         add(panel);
     }
@@ -104,7 +104,6 @@ public class Artist extends JFrame implements ActionListener {
     public static Artist getInstance(){
         if (instance == null){
             instance = new Artist();
-            //instance.setupTable();
         }
         return instance;
     }
@@ -133,134 +132,3 @@ public class Artist extends JFrame implements ActionListener {
 }
 
 
-
-
-/*
-
-
-
-public class Artist extends Canvas {
-    private static Artist instance = getInstance();
-    int windowWidth = 1400;
-    int windowHeight = 800;
-    int size = 120;     //domino size
-    FramePanels frame;
-    private Graphics graphics;
-
-    public static Artist getInstance(){
-        if (instance == null){
-            instance = new Artist();
-            instance.setupTable();
-        }
-        return instance;
-    }
-
-    public void drawDashes(int x, int y){
-        for (int i=0;i<5;i=i+2){
-            graphics.setColor(Color.black);
-            graphics.fillRoundRect(10+x+20*i,y,20,5,2,2);
-
-        }
-    }
-
-    private void setupTable(){
-        frame = new FramePanels("Awesome Dominoes", windowWidth, windowHeight);
-        frame.tableArea.add(instance);
-        graphics = getGraphics();
-    }
-
-    public void clearTable(){
-        graphics.setColor(Color.gray);
-        graphics.fillRect(0, 0, windowWidth, windowHeight);
-        frame.tableArea.update(graphics);
-
-    }
-
-    public void drawBone(int x,int y,Bone bone, String playerType){
-        if (playerType == "Player") {
-            drawBone(x,y,bone);
-        } else {
-            //TODO drawHiddenBone; //will replace below line when ready
-            drawBone(x,y,bone);
-        }
-    }
-
-    public void drawBone(int x,int y,Bone bone){
-        graphics.setColor(Color.black);
-        graphics.fillRoundRect(x, y, size,size/2,size/20,size/20);
-        drawPips(bone.left(),x,y,size);
-        graphics.setColor(Color.white);
-        graphics.drawLine(x+size/2, y, x+size/2, y+size/2);
-        drawPips(bone.right(),x+size/2,y,size);
-    }
-
-
-    private void drawPips(int num,int x,int y,int size){
-        switch(num){
-            case 1:drawOne(x,y,size);
-                break;
-            case 2:drawTwo(x,y,size);
-                break;
-            case 3:drawThree(x,y,size);
-                break;
-            case 4:drawFour(x,y,size);
-                break;
-            case 5:drawFive(x,y,size);
-                break;
-            case 6:drawSix(x,y,size);
-                break;
-        }
-    }
-
-
-    private void drawOne(int x,int y,int size){
-        graphics.setColor(Color.green);
-        graphics.fillOval(x+25, y+25, size/12, size/12);
-    }
-
-    private void drawTwo(int x,int y,int size){
-        graphics.setColor(Color.blue);
-        graphics.fillOval(x+10, y+10, size/12, size/12);
-        graphics.fillOval(x+40, y+40, size/12, size/12);
-    }
-
-    private void drawThree(int x,int y,int size){
-        graphics.setColor(Color.pink);
-        graphics.fillOval(x+10, y+10, size/12, size/12);
-        graphics.fillOval(x+25, y+25, size/12, size/12);
-        graphics.fillOval(x+40, y+40, size/12, size/12);
-    }
-
-    private void drawFour(int x,int y,int size){
-        graphics.setColor(Color.red);
-        graphics.fillOval(x+10, y+10, size/12, size/12);
-        graphics.fillOval(x+40, y+10, size/12, size/12);
-        graphics.fillOval(x+10, y+40, size/12, size/12);
-        graphics.fillOval(x+40, y+40, size/12, size/12);
-    }
-
-    private void drawFive(int x,int y,int size){
-        graphics.setColor(Color.cyan);
-        graphics.fillOval(x+25, y+25, size/12, size/12);
-        graphics.fillOval(x+10, y+10, size/12, size/12);
-        graphics.fillOval(x+40, y+10, size/12, size/12);
-        graphics.fillOval(x+10, y+40, size/12, size/12);
-        graphics.fillOval(x+40, y+40, size/12, size/12);
-    }
-
-    private void drawSix(int x,int y,int size){
-        graphics.setColor(Color.yellow);
-        graphics.fillOval(x+10, y+10, size/12, size/12);
-        graphics.fillOval(x+25, y+10, size/12, size/12);
-        graphics.fillOval(x+40, y+10, size/12, size/12);
-        graphics.fillOval(x+10, y+40, size/12, size/12);
-        graphics.fillOval(x+25, y+40, size/12, size/12);
-        graphics.fillOval(x+40, y+40, size/12, size/12);
-    }
-
-
-
-
-}
-
-*/
