ff832abbc5d3ae61f70dee1ad26d628dff831c01
Nick Mackin <git@greengrate.co.uk>
2013-03-12 12:31:19 +0000

    Player hands now centred

--- src/dominoes/PlayerHandPanel.java
+++ src/dominoes/PlayerHandPanel.java
@@ -18,6 +18,8 @@ public class PlayerHandPanel extends JPanel {
     //TODO use this.getWidth() and this.getHeight to place everthing in case we resize the window
 
     DominoPlayer player;
+    int size=120;
+    int boneGap=30;
 
     public PlayerHandPanel(FlowLayout flowLayout){
         super(flowLayout);
@@ -37,13 +39,14 @@ public class PlayerHandPanel extends JPanel {
     private void drawHand(Graphics graphics){
         //Represent the domino hands each player has.
         //Consider visible/not visible to human player
-        //TODO Put each into a frame above and below the central table frame within parent frame
         //Within each human hand, in order for the dominoes to be selectable, they will need to sit in a frame
         //This frame can glow when mouse-over events happen and playBone when clicked.
         Bone[] bones=player.bonesInHand();
+        int xStart=this.getWidth()/2-(size/2+boneGap)*(bones.length/2);
+        int y=getHeight()/2-size/2;
         for (int i=0; i<bones.length;i++){
             BoneDrawer boneDrawer = new BoneDrawer(bones[i], player.getClass().getName());
-            boneDrawer.drawBone(10 + 80 * i, 10, graphics);
+            boneDrawer.drawBone(xStart + (size/2+boneGap) * i, y, graphics);
             System.out.print(" ");
         }
     }
