c1376108581154201271442d28f7f10a0b9484ac
Timothy Baldock <tb@entropy.me.uk>
2013-03-19 09:59:25 +0000

    Cleanup

--- src/dominoes/players/Player.java
+++ src/dominoes/players/Player.java
@@ -5,8 +5,6 @@ import dominoes.*;
 import java.util.LinkedList;
 import java.util.List;
 
-import static java.lang.Thread.sleep;
-
 /**
  * Created with IntelliJ IDEA.
  * User: nick
@@ -14,7 +12,7 @@ import static java.lang.Thread.sleep;
  * Time: 21:54
  */
 public class Player implements DominoPlayer {
-    private String name = "new player";
+    private String name;
     private TurnCoordinator turnCoordinator;
     private int points = 0;
     protected List<Bone> hand = new LinkedList<Bone>();
@@ -30,61 +28,59 @@ public class Player implements DominoPlayer {
         this.turnCoordinator = turnCoordinator;
     }
 
-    public dominoes.Play makePlay(dominoes.Table table) throws dominoes.CantPlayException{
+
+    // Implementation of DominoPlayer interface
+    public dominoes.Play makePlay(dominoes.Table table) throws dominoes.CantPlayException {
         Play thePlay = chooseMove(table);
-        if (thePlay==null) throw new dominoes.CantPlayException();
+        if (thePlay == null) throw new dominoes.CantPlayException();
         hand.remove(thePlay.bone());
         return thePlay;
     }
 
-    protected dominoes.Play chooseMove(dominoes.Table table){
-        //not implemented yet
-        //nextMove.put(null);
-        turnCoordinator.getPlayerMove(this, nextMove);
-
-        return (Play)nextMove.get();
-    }
-
-
-    public void takeBack(dominoes.Bone bone){
+    public void takeBack(dominoes.Bone bone) {
         hand.add(bone);
     }
 
-    public void draw(dominoes.BoneYard boneYard){
-        Bone newBone=boneYard.draw();
-        if ( newBone != null ){
+    public void draw(dominoes.BoneYard boneYard) {
+        Bone newBone = boneYard.draw();
+        if (newBone != null) {
             hand.add(newBone);
         }
     }
 
-
-    public int numInHand(){
-         return hand.size();
+    public int numInHand() {
+        return hand.size();
     }
 
-    public dominoes.Bone[] bonesInHand(){
+    protected dominoes.Play chooseMove(dominoes.Table table) {
+        // Ask for a move to be made through the UI
+        turnCoordinator.getPlayerMove(this, nextMove);
+        // This will block until the other thread puts something here for us to retrieve
+        return (Play) nextMove.get();
+    }
+
+    public dominoes.Bone[] bonesInHand() {
         Bone[] b = hand.toArray(new Bone[0]);
         return b;
     }
 
-    public void newRound(){
-         hand.clear();
+    public void newRound() {
+        hand.clear();
     }
 
-    public void setPoints(int i){
+    public void setPoints(int i) {
         points = i;
     }
 
-    public int getPoints(){
+    public int getPoints() {
         return points;
     }
 
-    public void setName(String s){
-        name=s;
+    public void setName(String s) {
+        name = s;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-
 }
