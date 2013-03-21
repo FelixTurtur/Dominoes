6bfec364a24b64f2c20eb35d0c1cd89736f574d9
Timothy Baldock <tb@entropy.me.uk>
2013-03-21 16:47:04 +0000

    Cleanup

--- src/dominoes/Controller.java
+++ src/dominoes/Controller.java
@@ -1,12 +1,5 @@
 package dominoes;
 
-import dominoes.players.ComputerPlayer;
-import dominoes.players.DominoPlayer;
-import dominoes.players.Player;
-import dominoes.players.PlayerType;
-
-import javax.swing.*;
-
 /**
  * Created with IntelliJ IDEA.
  * User: nick
@@ -14,13 +7,8 @@ import javax.swing.*;
  * Time: 14:49
  */
 public class Controller {
-    //controls the running of the program
-
-    //parameters
-
     public static void main(String[] args) {
         UIFrame uiFrame = new UIFrame();
-        uiFrame.setVisible(true);
         uiFrame.showNewGameDialog();
     }
 }
